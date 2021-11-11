1069ff6dbd8277ec3ed9699e926286e0659aa0a5
Thomas Ismaïl Murzyn <thomasismailmurzyn@MacBook-Pro-de-Thomas.local>
2021-11-11 14:05:21 +0100

    writing a if condition if picture exist

--- routes/SignUp.js
+++ routes/SignUp.js
@@ -22,8 +22,13 @@ router.post("/user/signup", async (req, res) => {
       res.status(400).json({ message: "User account already exists" });
     } else {
       if (req.fields.username && req.fields.email && req.fields.password) {
-        let pictureToUpload = req.files.picture.path;
-        const resultUpload = await cloudinary.uploader.upload(pictureToUpload);
+        let pictureToUpload = req.files.picture.path || "";
+        if (pictureToUpload) {
+          const resultUpload = await cloudinary.uploader.upload(
+            pictureToUpload
+          );
+        }
+
         const password = req.fields.password;
         const salt = uid2(16);
         const hash = SHA256(password + salt).toString(encBase64);
@@ -33,7 +38,7 @@ router.post("/user/signup", async (req, res) => {
           account: {
             username: req.fields.username,
             phone: req.fields.phone,
-            avatar: resultUpload,
+            avatar: resultUpload ? resultUpload : "",
           },
           token: token,
           hash: hash,
