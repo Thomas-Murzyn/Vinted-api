c3fd5698538bf7a7455a7f3885cc232a82471e3b
Thomas Ismaïl Murzyn <thomasismailmurzyn@MacBook-Pro-de-Thomas.local>
2021-11-15 16:37:00 +0100

    adding the uploading picture process to a if

--- routes/SignUp.js
+++ routes/SignUp.js
@@ -38,11 +38,14 @@ router.post("/user/signup", async (req, res) => {
           salt: salt,
         });
 
-        const resultUpload = await cloudinary.uploader.upload(
-          req.files.picture.path
-        );
+        if (req.files.picture.path) {
+          const resultUpload = await cloudinary.uploader.upload(
+            req.files.picture.path
+          );
 
-        newUser.profil_picture = resultUpload;
+          newUser.profil_picture = resultUpload;
+        }
+
         const result = await newUser.save();
         res.status(200).json({
           message: "Account created",
