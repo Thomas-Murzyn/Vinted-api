e3b06af91751a742f33612aff8101e5d3cddbd4d
Thomas Ismaïl Murzyn <thomasismailmurzyn@MacBook-Pro-de-Thomas.local>
2021-11-16 17:58:50 +0100

    test

--- routes/Payment.js
+++ routes/Payment.js
@@ -5,9 +5,9 @@ const stripe = require("stripe")("sk_test_votreCléPrivée");
 const app = express();
 app.use(formidableMiddleware());
 
-router.post("payment", async (req, res) => {
+router.post("/payment", async (req, res) => {
   try {
-    console.log("payment");
+    console.log("/payment");
   } catch (error) {
     console.log(error);
   }
