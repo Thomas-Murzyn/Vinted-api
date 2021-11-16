acd18a20239335121aefa114de0281689bfa7f91
Thomas Ismaïl Murzyn <thomasismailmurzyn@MacBook-Pro-de-Thomas.local>
2021-11-16 22:35:05 +0100

    test

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 const cors = require("cors");
-// const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -32,24 +31,6 @@ app.use(offer);
 
 const payment = require("./routes/Payment");
 app.use(payment);
-
-// app.post("/payment", async (req, res) => {
-//   try {
-//     console.log("/payment");
-//     const response = await stripe.charges.create({
-//       amount: req.fields.price * 100,
-//       currency: "eur",
-//       description: req.fields.title,
-//       source: req.fields.stripeToken,
-//     });
-
-//     console.log(response.status);
-
-//     res.status(200).json(response);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
 
 app.listen(process.env.PORT, () => {
   console.log("Server has started ...");
